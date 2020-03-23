f0b4cb8f2ce290717da1115228111199f1ac7beb
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-23 15:03:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   nba_players = ["Lebron", "Giannis", "Kawhi", "Doncic"]
-  nba_players << "Harden"
+  nba_players << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
